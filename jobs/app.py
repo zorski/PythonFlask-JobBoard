a13e0b75d9577ebfd33d9aa0e7d5e248f5bd4918
Michal Zaorski <m.zaorski@icloud.com>
2020-06-15 20:38:01 +0200

    typo in execute.sql function

--- jobs/app.py
+++ jobs/app.py
@@ -16,7 +16,7 @@ def execute_sql(sql, values = (), commit = False, single = False):
     if commit:
         results = connection.commit()
     else:
-        results = cursor.fetchdone() if single else cursor.fetchall()
+        results = cursor.fetchone() if single else cursor.fetchall()
     cursor.close()
     return results
 
