c53c9fcae6664a0b1b3bed1910b379c5f00f92a9
Michal Zaorski <m.zaorski@icloud.com>
2020-06-15 21:33:14 +0200

    6 display individual jobs

--- jobs/app.py
+++ jobs/app.py
@@ -27,6 +27,10 @@ def close_connection(exception):
     if connection:
         connection.close()
 
+@app.route("/job/<job_id>")
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',[job_id], single=True)
+    return render_template("job.html", job=job)
 
 @app.route('/')
 @app.route('/jobs')
