1e9836b569a53fa7fefa20afea87b60f7419b816
Michal Zaorski <m.zaorski@icloud.com>
2020-06-16 17:48:58 +0200

    8

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,g
+from flask import Flask,render_template,g,redirect,url_for,request
+import datetime
 import sqlite3
 PATH = "db/jobs.sqlite"
 
@@ -33,6 +34,19 @@ def employer(employer_id):
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template("employer.html", employer=employer, jobs=jobs, reviews=reviews)
+
+@app.route("/employer/<employer_id>/review",methods=('GET', 'POST'))
+def review(employer_id):
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+        date = datetime.datetime.now().strftime("%d/%m/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',(review, rating, title, date, status, employer_id),commit=True)
+        return redirect(url_for("employer", employer_id=employer_id))
+    return render_template("review.html", employer_id=employer_id)
+
 
 @app.route("/job/<job_id>")
 def job(job_id):
